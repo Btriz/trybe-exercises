d327f20f7e9ef806b0952ce5af28bdd28786404e
btriz <00btrz@gmail.com>
2021-07-21 01:29:17 -0300

    removendo arquivos desnecessarios

---
+++ Front-end/bloco_11/dia_1/component/src/App.js
@@ -0,0 +1,7 @@
+import Eu from './Component'
+
+function App() {
+  return <Eu />
+}
+
+export default App;
--- Front-end/bloco_11/dia_1/meuapp/src/App.js
+++
@@ -1,8 +0,0 @@
-import './App.css';
-import Eu from './Component'
-
-function App() {
-  return <Eu />
-}
-
-export default App;
